2c31928f67bd255dc5a6de5edc7274746b80de82
dreaca <buddhikadreaca@gmail.com>
2025-02-18 14:01:03 +0530

    app settings

--- inventory-service/app.js
+++ inventory-service/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const {Eureka} = require("eureka-js-client")
+const {enabled} = require("express/lib/application");
+const e = require("express");
 
 const app = express();
 const port = 3000;
 
 const router = express.Router();
 
-router.get('/inventory ',(req,res)=>{
+router.get('/inventory',(req,res)=>{
     res.json(
         {
             items:['Milk','Eggs','Bread'],
@@ -14,6 +17,39 @@ router.get('/inventory ',(req,res)=>{
     )
 })
 app.use('/inventory-service', router);
+
+const eurekaClient = new Eureka({
+    instance: {
+        instanceId : "inventory-service",
+        app : "INVENTORY_SERVICE",
+        hostName : "localhost",
+        ipAddr: "127.0.0.1",
+        port:{
+            $: port,
+            "@enabled": true
+        },
+        vipAddress: "inventory-service",
+        dataCenterInfo:{
+            "@class": "com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo",
+            name: "MyOwn"
+        }
+    },
+
+    eureka: {
+        host: "localhost",
+        port: 8761,
+        // servicePath: "/eureka/"
+        //http://localhost:8761/
+    }
+})
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
+    eurekaClient.start((err)=>{
+        if (err) {
+            console.log(err, "Fail to register eureka");
+        }else {
+            console.log("Successfully registered")
+        }
+    });
 })
--- order-service/src/main/java/lk/ijse/gdse/orderservice/controller/OrderController.java
+++ order-service/src/main/java/lk/ijse/gdse/orderservice/controller/OrderController.java
@@ -1,12 +1,13 @@
 package lk.ijse.gdse.orderservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
-
+    @GetMapping("/all")
     public String getOrders(){
         return "Order Service : Order Service1, Order2, Order3, Order4";
     }
